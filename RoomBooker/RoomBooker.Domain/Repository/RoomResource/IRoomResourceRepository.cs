08ffd931c469e901c103ef386139e047f5f7f0cd
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-20 20:25:22 -0300

    feat: criando a interface do respository: IRoomResourceRepository

---
+++ RoomBooker/RoomBooker.Domain/Repository/RoomResource/IRoomResourceRepository.cs
@@ -0,0 +1,14 @@
+using RoomBooker.Domain.Entity.RoomResource;
+using RoomBooker.Domain.Entity.RoomResource.Request;
+
+
+namespace RoomBooker.Domain.Repository.RoomResource
+{
+    public interface IRoomResourceRepository
+    {
+        Task<int> CreateRoomResource(RoomResourceInfo roomResource);
+        Task UpdateRoomResource(int id, RoomResourceInfo roomResource);
+        Task DeleteRoomResource(int id);
+        Task<List<RoomResourceInfo>> SelectRoomResource(RoomResourceRequest request);
+    }
+}
