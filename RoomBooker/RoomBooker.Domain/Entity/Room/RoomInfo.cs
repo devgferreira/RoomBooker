67522fe126a9bfc92f1e27fd34da6f2f2538c903
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-20 19:56:17 -0300

    feat: criando a entidade RoomInfo

---
+++ RoomBooker/RoomBooker.Domain/Entity/Room/RoomInfo.cs
@@ -0,0 +1,9 @@
+namespace RoomBooker.Domain.Entity.Room
+{
+    public class RoomInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Capacity { get; set; }
+    }
+}
