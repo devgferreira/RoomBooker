eda404ef616c1ccc3b5adec061e3cc4fbae5e0dd
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-20 20:51:19 -0300

    feat: criando o request: BookingRequest

---
+++ RoomBooker/RoomBooker.Domain/Entity/Booking/Request/BookingRequest.cs
@@ -0,0 +1,12 @@
+namespace RoomBooker.Domain.Entity.Booking.Request
+{
+    public class BookingRequest
+    {
+        public int? Id { get; set; }
+        public int? UserId { get; set; }
+        public int? RoomId { get; set; }
+        public DateTime? InitialDate { get; set; }
+        public DateTime? FinalDate { get; set; }
+        public DateTime? Day { get; set; }
+    }
+}
