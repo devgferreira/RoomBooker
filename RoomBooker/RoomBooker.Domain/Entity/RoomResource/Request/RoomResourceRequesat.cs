14be9f24ebc9594e497b668880d6c25977a50cc5
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-20 21:02:19 -0300

    feat: mudando a entidade: RoomResourceInfo

--- RoomBooker/RoomBooker.Domain/Entity/RoomResource/Request/RoomResourceRequesat.cs
+++ RoomBooker/RoomBooker.Domain/Entity/RoomResource/Request/RoomResourceRequesat.cs
@@ -3,5 +3,7 @@ namespace RoomBooker.Domain.Entity.RoomResource.Request
     public class RoomResourceRequest
     {
         public int? Id { get; set; }
+        public int? RoomId { get; set; }
+        public int? ResourceId { get; set; }
     }
 }
--- RoomBooker/RoomBooker.Domain/Entity/RoomResource/RoomResourceInfo.cs
+++ RoomBooker/RoomBooker.Domain/Entity/RoomResource/RoomResourceInfo.cs
@@ -3,6 +3,7 @@ namespace RoomBooker.Domain.Entity.RoomResource
     public class RoomResourceInfo
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public int RoomId { get; set; }
+        public int ResourceId { get; set; }
     }
 }
