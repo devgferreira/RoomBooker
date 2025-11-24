8f2ac1a814da4d6e6655ba8d901977f695fd04d9
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-24 12:19:59 -0300

    feat: criando o Context para se conectar ao banco de dados

---
+++ RoomBooker/RoomBooker.Infra.Data/Context/DbContext .cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace RoomBooker.Infra.Data.Context
+{
+    public class DbContext : IDisposable
+    {
+        public IDbConnection Connection { get; set; }
+        public DbContext(IConfiguration configuration)
+        {
+            var connStr = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+                  ?? configuration.GetConnectionString("DefaultConnection");
+
+            Connection = new MySqlConnection(connStr);
+            Connection.Open();
+        }
+        public void Dispose() => Connection?.Dispose();
+    }
+}
