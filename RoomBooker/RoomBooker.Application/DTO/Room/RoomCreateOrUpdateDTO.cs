72c75424a5de23587000ebb4a453fa123d969b06
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-24 14:36:18 -0300

    feat: criando DTOs para a entidade Room

---
+++ RoomBooker/RoomBooker.Application/DTO/Room/RoomCreateOrUpdateDTO.cs
@@ -0,0 +1,7 @@
+namespace RoomBooker.Application.DTO.Room;
+
+public class RoomCreateOrUpdateDTO
+{
+    public string Name { get; set; }
+    public int Capacity { get; set; }
+}
---
+++ RoomBooker/RoomBooker.Application/DTO/Room/RoomDTO.cs
@@ -0,0 +1,8 @@
+namespace RoomBooker.Application.DTO.Room;
+
+public class RoomDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Capacity { get; set; }
+}
