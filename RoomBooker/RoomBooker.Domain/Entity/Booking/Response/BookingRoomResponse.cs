4076a4eaebf197c16c186dbc2e756f058fdf9df8
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-25 15:20:37 -0300

    feat: criando o response: BookingRoomResponse

---
+++ RoomBooker/RoomBooker.Domain/Entity/Booking/Response/BookingRoomResponse.cs
@@ -0,0 +1,16 @@
+namespace RoomBooker.Domain.Entity.Booking.Response
+{
+    public class BookingRoomResponse
+    {
+        public int BookingId { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public string RoomCapacity { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime InitialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+        public DateTime Day { get; set; }
+    }
+}
