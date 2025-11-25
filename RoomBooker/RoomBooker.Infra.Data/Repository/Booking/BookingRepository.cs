bf2fa362e708638d96404e7eb93d376d33a89ee4
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-25 15:27:39 -0300

    feat: implementando o método: SelectBookingRoom - BookingRepository

--- RoomBooker/RoomBooker.Infra.Data/Repository/Booking/BookingRepository.cs
+++ RoomBooker/RoomBooker.Infra.Data/Repository/Booking/BookingRepository.cs
@@ -1,6 +1,7 @@
 using RoomBooker.Domain.Interface.Booking;
 using RoomBooker.Domain.Entity.Booking;
 using RoomBooker.Domain.Entity.Booking.Request;
+using RoomBooker.Domain.Entity.Booking.Response;
 using RoomBooker.Infra.Data.Context;
 using Dapper;
 
@@ -18,21 +19,22 @@ namespace RoomBooker.Infra.Data.Repository.Booking
 
         public async Task<int> CreateBooking(BookingInfo booking)
         {
-            var sql = "INSERT INTO Bookings (RoomId, InitialDate, FinalDate, UserId) VALUES (@RoomId, @InitialDate, @FinalDate, @UserId)";
+            var sql = "INSERT INTO Booking (RoomId, InitialDate, FinalDate, UserId, Day) VALUES (@RoomId, @InitialDate, @FinalDate, @UserId, @Day)";
             var id = await _context.Connection.ExecuteScalarAsync<int>(sql,
                 new
                 {
                     RoomId = booking.RoomId,
                     InitialDate = booking.InitialDate,
                     FinalDate = booking.FinalDate,
-                    UserId = booking.UserId
+                    UserId = booking.UserId,
+                    Day = booking.Day
                 });
             return id;
         }
 
         public async Task UpdateBooking(int id, BookingInfo booking)
         {
-            var sql = "UPDATE Bookings SET RoomId = @RoomId, InitialDate = @InitialDate, FinalDate = @FinalDate, UserId = @UserId WHERE Id = @Id";
+            var sql = "UPDATE Booking SET RoomId = @RoomId, InitialDate = @InitialDate, FinalDate = @FinalDate, UserId = @UserId, Day = @Day WHERE Id = @Id";
             await _context.Connection.ExecuteAsync(sql,
                 new
                 {
@@ -40,19 +42,65 @@ namespace RoomBooker.Infra.Data.Repository.Booking
                     RoomId = booking.RoomId,
                     InitialDate = booking.InitialDate,
                     FinalDate = booking.FinalDate,
-                    UserId = booking.UserId
+                    UserId = booking.UserId,
+                    Day = booking.Day
                 });
         }
 
         public async Task DeleteBooking(int id)
         {
-            var sql = "DELETE FROM Bookings WHERE Id = @Id";
+            var sql = "DELETE FROM Booking WHERE Id = @Id";
             await _context.Connection.ExecuteAsync(sql, new { Id = id });
         }
 
         public async Task<List<BookingInfo>> SelectBooking(BookingRequest request)
         {
-            var sql = "SELECT * FROM Bookings WHERE 1 = 1 ";
+            var sql = "SELECT * FROM Booking WHERE 1 = 1 "; // ajustar o * com os campos certos
+            if (request.RoomId != null)
+            {
+                sql += "AND RoomId = @RoomId ";
+            }
+            if (request.UserId != null)
+            {
+                sql += "AND UserId = @UserId ";
+            }
+            if (request.InitialDate != null && request.FinalDate != null)
+            {
+                sql += "AND InitialDate >= @InitialDate ";
+            }
+            if (request.FinalDate != null)
+            {
+                sql += "AND FinalDate <= @FinalDate ";
+            }
+            if (request.Id != null)
+            {
+                sql += "AND Id = @Id ";
+            }
+            if (request.Day != null)
+            {
+                sql += "AND Day = @Day ";
+            }
+            var bookings = await _context.Connection.QueryAsync<BookingInfo>(sql,
+                new
+                {
+                    Id = request.Id,
+                    RoomId = request.RoomId,
+                    InitialDate = request.InitialDate,
+                    FinalDate = request.FinalDate,
+                    UserId = request.UserId,
+                    Day = request.Day
+                });
+            return bookings.ToList();
+        }
+
+        public async Task<List<BookingRoomResponse>> SelectBookingRoom(BookingRequest request)
+        {
+            var sql = "SELECT * FROM Booking WHERE 1 = 1 "; // ajustar o * com os campos certos
+
+            if (request.UserId != null)
+            {
+                sql += "AND UserId = @UserId ";
+            }
             if (request.RoomId != null)
             {
                 sql += "AND RoomId = @RoomId ";
@@ -69,13 +117,19 @@ namespace RoomBooker.Infra.Data.Repository.Booking
             {
                 sql += "AND Id = @Id ";
             }
-            var bookings = await _context.Connection.QueryAsync<BookingInfo>(sql,
+            if (request.Day != null)
+            {
+                sql += "AND Day = @Day ";
+            }
+            var bookings = await _context.Connection.QueryAsync<BookingRoomResponse>(sql,
                 new
                 {
                     Id = request.Id,
                     RoomId = request.RoomId,
                     InitialDate = request.InitialDate,
-                    FinalDate = request.FinalDate
+                    FinalDate = request.FinalDate,
+                    UserId = request.UserId,
+                    Day = request.Day
                 });
             return bookings.ToList();
         }
