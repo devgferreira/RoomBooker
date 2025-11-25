1abdd7ea044a9d6e78613cb42332fb92ecd587ea
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-25 13:30:57 -0300

    feat: criando a interface: IBookingService

---
+++ RoomBooker/RoomBooker.Application/Interface/Booking/IBookingService.cs
@@ -0,0 +1,12 @@
+using RoomBooker.Application.DTO.Booking;
+using RoomBooker.Domain.Entity.Booking.Request;
+
+namespace RoomBooker.Application.Interface.Booking
+{
+    public interface IBookingService
+    {
+        Task CreateBookingAsync(BookingCreateDTO bookingCreateDTO);
+        Task DeleteBookingAsync(int id);
+        Task<List<BookingDTO>> SelectBookingAsync(BookingRequest request);
+    }
+}
