cf0d5af8fb38c2e0c128067cb49198ed48c65ae2
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-25 15:20:59 -0300

    feat: adicionando o contrato: SelectBookingRoom - IBookingRepository

--- RoomBooker/RoomBooker.Domain/Interface/Booking/IBookingRepository.cs
+++ RoomBooker/RoomBooker.Domain/Interface/Booking/IBookingRepository.cs
@@ -1,5 +1,6 @@
 using RoomBooker.Domain.Entity.Booking;
 using RoomBooker.Domain.Entity.Booking.Request;
+using RoomBooker.Domain.Entity.Booking.Response;
 
 namespace RoomBooker.Domain.Interface.Booking
 {
@@ -9,5 +10,6 @@ namespace RoomBooker.Domain.Interface.Booking
         Task UpdateBooking(int id, BookingInfo booking);
         Task DeleteBooking(int id);
         Task<List<BookingInfo>> SelectBooking(BookingRequest request);
+        Task<List<BookingRoomResponse>> SelectBookingRoom(BookingRequest request);
     }
 }
