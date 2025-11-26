fc32b9fa9a7964eab438be6e6997db690331984e
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-11-26 12:26:48 -0300

    feat: criando a controller do Room

---
+++ RoomBooker/RoomBooker.API/Controller/Room/RoomController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using RoomBooker.Application.DTO.Room;
+using RoomBooker.Application.Interface.Room;
+using RoomBooker.Domain.Entity.Room.Request;
+
+namespace RoomBooker.API.Controller.Room
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRoom([FromBody] RoomCreateOrUpdateDTO room)
+        {
+            await _roomService.CreateRoomAsync(room);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateRoom([FromQuery] int id, [FromBody] RoomCreateOrUpdateDTO room)
+        {
+            await _roomService.UpdateRoomAsync(id, room);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteRoom([FromQuery] int id)
+        {
+            await _roomService.DeleteRoomAsync(id);
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetRooms([FromQuery] RoomRequest request)
+        {
+            var rooms = await _roomService.SelectRoomAsync(request);
+            return Ok(rooms);
+        }
+    }
+}
