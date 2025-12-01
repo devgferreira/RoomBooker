579fc1a92c9bc5f2f59a234f768bb306d52b8a0f
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-01 20:03:07 -0300

    feat: criando o response: RoomWithResourceResponse

---
+++ RoomBooker/RoomBooker.Domain/Entity/Room/Response/RoomWithResourceResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBooker.Domain.Entity.Room.Response
+{
+    public class RoomWithResourceResponse
+    {
+        public int RoomId { get; set; }
+
+        public string RoomName { get; set; }
+        public int RoomCapacity { get; set; }
+        public int ResourceId { get; set; }
+        public string ResourceName { get; set; }
+        public int ResourceCapacity { get; set; }
+
+    }
+}
