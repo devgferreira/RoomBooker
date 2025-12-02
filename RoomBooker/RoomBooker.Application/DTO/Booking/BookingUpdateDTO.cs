f5226043c00aabaf2ea6d2f835ed227c62d06ed7
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-02 19:22:48 -0300

    feat: criando o dto: BookingUpdateDTO

---
+++ RoomBooker/RoomBooker.Application/DTO/Booking/BookingUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RoomBooker.Application.DTO.Booking
+{
+    public class BookingUpdateDTO
+    {
+        public int UserId { get; set; }
+        public DateTime InitialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+    }
+}
