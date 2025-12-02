5196332240cf3d934bad879c83c2d43eec9b7f66
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-02 00:05:33 -0300

    feat: criando a controller do BookingController

---
+++ RoomBooker/RoomBooker.API/Controller/Booking/BookingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoomBooker.Application.DTO.Booking;
+using RoomBooker.Application.DTO.Resource;
+using RoomBooker.Application.Interface.Booking;
+
+namespace RoomBooker.API.Controller.Booking
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+
+        public BookingController(IBookingService bookingService) {
+            _bookingService = bookingService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking([FromBody] BookingCreateDTO bookingDTO)
+        {
+            await _bookingService.CreateBookingAsync(bookingDTO);
+            return Ok();
+        }
+    }
+}
